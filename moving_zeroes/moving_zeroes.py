a0f5378cf630b5b1f8924e4eeba0f05fc68bc32f
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-09-10 12:02:41 -0500

    first pass algorithms

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,25 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# To do: 
+#Check values of array
+#If array value is not a 0, move it to the left side of the array
+# all 0's should be at the end if any
+#Order doesn't matter for non zeros
+
+
+#make loop over input array
+#check int value
+#pop or push non zero to front on pass?
+
 def moving_zeroes(arr):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            value = arr.pop(i)
+            arr.insert(0, value)
 
-    pass
+    return arr
 
 
 if __name__ == '__main__':
